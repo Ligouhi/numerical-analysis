d89273626c145995562416f7363ed3bdab67fd59
ligohi <1175069074@qq.com>
2019-01-05 15:24:09 +0800

    fix

--- liuen.py
+++ liuen.py
@@ -17,6 +17,9 @@ def func_deri(x0):
     return a.evalf(subs = {x:x0})
 
 #画函数图
+plt.style.use('fivethirtyeight')
+plt.rcParams["font.sans-serif"] = ["SimHei"]
+plt.rcParams['axes.unicode_minus'] = False
 xs = np.arange(0,4,0.1)
 y = []
 for i in xs:
@@ -28,7 +31,7 @@ plt.show()
 
 x0 = float(input("input initialize x:"))
 d = float(input("input step length:"))
-
+#修恩法
 y_pre = []
 x_pre = []
 tmp = x0
@@ -39,17 +42,39 @@ for i in range(0,lens):
     
     k = (func_deri(tmp)+func_deri(tmp+d))/2
     y_pre.append(y_pre[i]+k*d)
-    print(k*d,func(tmp+d))
     tmp+=d
     x_pre.append(tmp)
+    
+#欧拉法
+y2_pre = []
+x2_pre = []
+tmp = x0
+x2_pre.append(tmp)
+y2_pre.append(func(tmp))
+lens = int(4/d)
+for i in range(0,lens): 
+    
+    k = func_deri(tmp)
+    y2_pre.append(y2_pre[i]+k*d)
+   
+    tmp+=d
+    x2_pre.append(tmp)   
+    
 #画预测函数图
 xs = np.arange(x0,x0+4,0.1)
 y = []
 for i in xs:
     y.append(func(i))
-plt.plot(xs,y)
-plt.plot(x_pre,y_pre)
+plt.plot(xs,y,label='原函数')
+plt.plot(x2_pre,y2_pre,label='欧拉法')
+plt.plot(x_pre,y_pre,label='修恩法')
+plt.title('求解函数 f(x) = -0.5x^4+4x^3-10x^2+8.5x+1')
 for i in range(0,len(x_pre)):
     plt.scatter(x_pre,y_pre)
-
+for i in range(0,len(x2_pre)):
+    plt.scatter(x2_pre,y2_pre)
+plt.legend(['欧拉法','修恩法','原函数'])
+plt.legend()
 plt.show()
+
+print('1607094245 任腾')
